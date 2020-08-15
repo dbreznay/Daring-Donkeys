c0b253693da281c11d5ce8335cda4c14bb768609
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-08-15 10:31:28 -0500

    added conrtoller route for handlebars

---
+++ controllers/handlebars_controllers.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+
+var employee = require("../models/employee");
+
+router.get("/", (req, res) => {
+    // console.log(req);
+
+        res.render("employees", hbsObject);
+    });
+
+
+// router.post("/api/burger", (req, res) => {
+//     burger.insertOne("burger_name", req.body.burger_name, result => {
+//         res.json({id: result.insertId});
+//     });
+// });
+
+// router.put("/api/burger/:id", (req, res) => {
+//     //creates id = id
+//     var burgerId = req.params.id;
+  
+//     burger.updateOne(burgerId, result => {
+//       if (result.changedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
+// Export routes for server.js to use.
+module.exports = router;
