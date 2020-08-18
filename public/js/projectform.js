9c0a6af914bfa59ff02530f15e57817ab3f4b72a
Kyle <kjknudtson1989@gmail.com>
2020-08-18 17:45:08 -0500

    Added api routes for adding employees and projects and viewing employees info.  Also adjusted some CSS

---
+++ public/js/projectform.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    var projInput = $("#proj-name");
+    var clientInput = $("#client-name");
+    var projNum = $("#project-number");
+    var start = $("#start-date");
+    var end = $("#end-date");
+
+    $(document).on("submit", "#project-form", handleProjectFormSubmit);
+
+    function handleProjectFormSubmit(event) {
+        event.preventDefault();
+
+        if (!projInput.val().trim().trim() || !clientInput.val().trim().trim() || !projNum.val().trim().trim()) {
+            return;
+        }
+
+        insertProject({
+            name: projInput
+                .val()
+                .trim()
+            ,
+                        
+            number: projNum
+                .val()
+                .trim()
+            ,
+            client: clientInput
+                .val()
+                .trim()
+            ,
+            start: start
+                .val()
+                .trim()
+            ,
+            end: end
+                .val()
+                .trim()
+        });
+
+    function insertProject(projectData) {
+        $.post("/api/project", projectData)
+          .then(function() {
+              console.log("Project Added!");
+          });
+    }
+
+
+
+    }
+});
